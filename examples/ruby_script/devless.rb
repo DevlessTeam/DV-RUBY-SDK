c473ef88b9c934a32d961bd1a73431b2eeff5ce9
bkwaku <bkwaku@rocketmail.com>
2017-04-27 20:36:30 +0000

    Make search work and made request_processor private

--- examples/ruby_script/devless.rb
+++ examples/ruby_script/devless.rb
@@ -16,25 +16,26 @@ let's do this!
 =end
 
 #require the gem
-require "DV_RUBY_SDK"
+require_relative "../../lib/DV_RUBY_SDK"
 
 =begin
 set it up with your credentials. you will need these two credentials {token}
 token can be found in the app tab on your devless panel where you created your services
 copy the token.
-my token  = "7740b4b2303e32957a3215c344b8c21c"
+my token  = "63946647b14d4ab9679cb6d8aa9bbb05"
 You will also need the {url} ie the url of the devless app you created
 (in my case the one I created and hosted on heroku)
-my_app_url = "http://newerapper.herokuapp.com"
+my_app_url = "http://127.0.0.1"
 If you have these two you can set up
 set it up this way
 =end
 
-my_token = "7740b4b2303e32957a3215c344b8c21c"
-my_app_url =  "http://newerapper.herokuapp.com"
+my_token = "63946647b14d4ab9679cb6d8aa9bbb05"
+my_app_url =  "http://127.0.0.1"
 
 DVRUBYSDK.token = my_token
 DVRUBYSDK.url   = my_app_url
+DVRUBYSDK.port  = 4040
 
 #create an intance of the devless class and assign it to a varibale.
 # I will call mine *devless* you could call yours anything.
@@ -44,17 +45,17 @@ devless = DVRUBYSDK::Devless.new
 #at this stage we can query data, post data, patch and do what ever we want to our table from here
 #lets first query data
 
-service_name = "names"
-table_name = "eit_names"
+service_name = "dentist"
+table_name = "dentist_list"
 
-query_response = devless.query_data(service_name, table_name)
+query_response = devless.search(service_name, table_name,"name","dhsfhjsdhjjhj")
 puts query_response
 
 
 #lets post data to that same table called "eit_names" which require one field ie "name"
-data_we_want_to_post = {name: "Charles"}
-post_data_response = devless.add_data(service_name, table_name, data_we_want_to_post)
-puts post_data_response
+#data_we_want_to_post = {name: "Charles"}
+#post_data_response = devless.add_data(service_name, table_name, data_we_want_to_post)
+#puts post_data_response
 
 =begin
 Run it by pressing command b on your sublime if youre on a mac or run the script in your terminal if otherwise.
